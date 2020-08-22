64d17446bcb1b00481d4f5daf62a7912b739b415
Hiran Sarkar <63890677+hiransarkar21@users.noreply.github.com>
2020-08-22 14:06:58 +0530

    Update Dice Rolling Simulator.py

--- Dice Rolling Simulator.py
+++ Dice Rolling Simulator.py
@@ -20,7 +20,7 @@ class MainWindow(QWidget):
 
         self.setFixedHeight(600)
         self.setFixedWidth(600)
-        self.setIcon(QIcon("icon.png"))
+        self.setWindowTitle("Dice Rolling Simulator")
 
     def user_interface(self):
 
